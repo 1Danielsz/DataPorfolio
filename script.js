c67cce99969094a1f7f2e0bd5c2e4dacb89a0709
Daniel Lira de Souza <daniel.souza@exyon.local>
2022-12-29 04:43:38 -0300

    Added some Javascript

---
+++ script.js
@@ -0,0 +1,27 @@
+var mySwiper = new Swiper('.mySwiper', {
+    slidesPerView: 3,
+    spaceBetween: 30,
+    loop: true,
+    slidesPerGroup: 3,
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+    pagination: {
+      el: '.swiper-pagination',
+      type: 'bullets',
+      clickable: true,
+    },
+    speed: 1000,
+  });
+  
+  
+
+  /*
+  
+  
+  
+  
+  
+  */
+  
